386c73a0b38012c9c46ecd4395ddfa449ff00d04
Vaibhav Reddy <vaibhavreddy0852@gmail.com>
2023-01-21 21:43:25 +0530

    Removed unwated packages

--- main.py
+++ main.py
@@ -1,13 +1,10 @@
 from email.mime.text import MIMEText
 from email.mime.multipart import MIMEMultipart
 from email.mime.base import MIMEBase
-from email import encoders
 import smtplib
-import ssl
 import os
 import imaplib
 import email
-import traceback
 import datetime
 
 
