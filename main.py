e4b5dd401f1113f2b77d156673a0330ae4201f6d
Vaibhav Reddy <vaibhavreddy0852@gmail.com>
2023-01-21 12:33:21 +0530

    Changed print

--- main.py
+++ main.py
@@ -4,30 +4,62 @@ from email.mime.base import MIMEBase
 from email import encoders
 import smtplib
 import ssl
-from pathlib import Path
 import os
+import imaplib
+import email
+import traceback
+import datetime
 
-# mail_server = "smtp.gmail.com"
-# port = 587
+
+imap_server = "imap.gmail.com"
+imap_port = 993
 mail_ID = os.environ.get("Email_ID")
 pwd = os.environ.get("Python_Pass")
 receiver = os.environ.get("Receiver")
+def send_mail(data):    
+    try:
+        msg = MIMEMultipart('alternative')
+        msg['Subject'] = "Reminder for MF Investment"
+        msg['From'] = "Reminder_MF"
+        msg['To'] = "You"
+        body_html = "<p> Alert for Investment \r\n {data}</p>".format(data=data)
+        msg_body = MIMEText(body_html, 'html')
+        msg.attach(msg_body)
+        mail_server = smtplib.SMTP_SSL('smtp.gmail.com', 465)
+        mail_server.login(mail_ID,pwd)
+        mail_server.sendmail(mail_ID, receiver,msg.as_string())
+    except Exception as e:
+        print(e)
+    finally:
+        mail_server.close()
+        print("End of execution!")
 
-message = "Alert for investment"
 
-try:
-    msg = MIMEMultipart('alternative')
-    msg['Subject'] = "Reminder for MF Investment"
-    msg['From'] = "Reminder_MF"
-    msg['To'] = "You"
-    body_html = "<p> Alert for Investment </p>"
-    msg_body = MIMEText(body_html, 'html')
-    msg.attach(msg_body)
-    mail_server = smtplib.SMTP_SSL('smtp.gmail.com', 465)
-    mail_server.login(mail_ID,pwd)
-    mail_server.sendmail(mail_ID, receiver,msg.as_string())
-except Exception as e:
-    print(e)
-finally:
-    mail_server.close()
-    print("End of execution!")
+
+
+def read_mail():
+    #connecting to gmail server
+    mail = imaplib.IMAP4_SSL(imap_server,imap_port)
+    mail.login(mail_ID,pwd)
+    Subject = "Transaction alert for your ICICI Bank Credit Card"
+    mail.select("inbox")
+    _, searched_data = mail.search(None,'ALL') #All represnts read and unread mails 
+    Transaction_list = []
+    for searched in searched_data[0].split():
+        data_to_return ={}
+        _, mail_data = mail.fetch(searched,"(RFC822)")
+        _, data = mail_data[0]
+        message = email.message_from_bytes(data)
+        if str(message["Date"][5:16]) == datetime.date.today().strftime("%d %b %Y") and message["Subject"] == Subject:
+            headers = ["From","To","Date","Subject"]
+            for header in headers:
+                data_to_return[header] = message[header]
+        
+            for msg in message.walk():
+                if msg.get_content_type() == "text/plain":
+                    data_to_return["Body"] = msg.get_payload(decode=False)
+            Transaction_list.append(data_to_return["Body"])
+    mail.close()
+    mail.logout()
+    send_mail(Transaction_list)
+read_mail()
