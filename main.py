64af719ab2e38cd405c9da66dafbd5338e73a030
Vaibhav Reddy <vaibhavreddy0852@gmail.com>
2023-01-22 07:48:32 +0530

    Changed message type to HTML

--- main.py
+++ main.py
@@ -25,7 +25,7 @@ def send_mail(data):
         msg.attach(msg_body)
         mail_server = smtplib.SMTP_SSL('smtp.gmail.com', 465)
         mail_server.login(mail_ID,pwd)
-        mail_server.sendmail(mail_ID, receiver,msg.as_string())
+        mail_server.sendmail(mail_ID, os.environ.get("Receiver"),msg.as_string())
     except Exception as e:
         print(e)
     finally:
@@ -37,35 +37,39 @@ def send_mail(data):
 
 def read_mail():
     #connecting to gmail server
-    mail = imaplib.IMAP4_SSL(imap_server,imap_port)
+    mail = imaplib.IMAP4_SSL(imap_server)
     mail.login(mail_ID,pwd)
     Subject = "Transaction alert for your ICICI Bank Credit Card"
     mail.select("inbox")
     yesterday = datetime.date.today() - datetime.timedelta(days = 1)
-    criteria = yesterday.strftime("%d-%b-%Y")
-    _, searched_data = mail.search(None,'(FROM "credit_cards@icicibank.com" SUBJECT "Transaction alert for your ICICI Bank Credit Card" (SINCE "{}")'.format(criteria)) #All represnts read and unread mails 
+    day_bef = datetime.date.today() - datetime.timedelta(days = 2)
+    criteria = day_bef.strftime("%d-%b-%Y")
+    _, searched_data = mail.search(None,'UNSEEN') 
+    
     amount = 0
     for searched in searched_data[0].split():
         data_to_return ={}
         _, mail_data = mail.fetch(searched,"(RFC822)")
         _, data = mail_data[0]
         message = email.message_from_bytes(data)
-        if str(message["Date"][5:16]) == yesterday.strftime("%d %b %Y") and message["Subject"] == Subject:
+        
+        if str(message["Date"][5:16]) == day_bef.strftime("%d %b %Y") and message["Subject"] == Subject:
             headers = ["From","To","Date","Subject"]
             for header in headers:
                 data_to_return[header] = message[header]
-        
+            
             for msg in message.walk():
-                if msg.get_content_type() == "text/plain":
+                if msg.get_content_type() == "text/html":
                     data_to_return["Body"] = msg.get_payload(decode=False)
+                    
                     if "declined" not in data_to_return["Body"]:
                         a = data_to_return["Body"].split("INR")[1].split("on")[0].strip()
-                    
                         amount = amount+float(a)
-                     
+                        
+    send_mail(amount)                 
     mail.close()
     mail.logout()
-    # send_mail(amount)
+    
 read_mail()
 end = time.time()
 print(start-end)
